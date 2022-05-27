d1bd1fa9ca58bd7d3a7bba4fb1197c75e71cb3ed
Andrew Shepherd <shepherd_a@hotmail.com>
2022-05-28 05:15:54 +1000

    Solved russion doll envelopes

--- russian-doll-envelopes/russian_doll_envelopes.py
+++ russian-doll-envelopes/russian_doll_envelopes.py
@@ -1,12 +1,22 @@
-def getMaxLength(node):
-    if node["maxLength"] != None:
-        return node["maxLength"]
-    maxLength = 1
-    joining = node["joining"]
-    if joining:
-        maxLength += max([getMaxLength(n) for n in joining])
-    node["maxLength"] = maxLength
-    return maxLength
+
+
+def lis(values):
+    # Start point, length
+    if len(values) == 1:
+        return 1
+    entries = [[values.pop(0), 1]]
+    
+    for v in values:
+        if v < entries[0][0]:
+            entries[0][0] = v
+        elif v > entries[-1][0]:
+            entries.append([v, entries[-1][1] + 1])
+        else:
+            for i in range(1, len(entries)):
+                if v > entries[i-1][0] and v <= entries[i][0]:
+                    entries[i] = [v, i+1]
+                    break
+    return len(entries) 
 
 class Solution(object):
     def maxEnvelopes(self, envelopes):
@@ -14,16 +24,17 @@ class Solution(object):
         :type envelopes: List[List[int]]
         :rtype: int
         """
-        nodes = [{ "joining": [], "maxLength": None} for e in envelopes]
-        for i in range(len(envelopes) - 1):
-            for j in range(i+1, len(envelopes)):
-                iWidth, iHeight = envelopes[i]
-                jWidth, jHeight = envelopes[j]
-                if iWidth < jWidth:
-                    if iHeight < jHeight:
-                        nodes[i]["joining"].append(nodes[j])
-                elif jWidth < iWidth:
-                    if jHeight < iHeight:
-                        nodes[j]["joining"].append(nodes[i])
-        return max([getMaxLength(n) for n in nodes])
+        if(len(envelopes) == 0):
+            return 0
+        envelopes = sorted(envelopes, key=lambda e:[e[0], e[1]*-1])
+        i = 1
+        while i < len(envelopes):
+            if envelopes[i] == envelopes[i-1]:
+                envelopes.pop(i)
+            else:
+                i += 1
         
+        if(len(envelopes) == 1):
+            return 1
+
+        return lis([e[1] for e in envelopes])
--- russian-doll-envelopes/test.py
+++ russian-doll-envelopes/test.py
@@ -10,15 +10,22 @@ class TestSolution(unittest.TestCase):
         result = s.maxEnvelopes(envelopes)
         self.assertEqual(result, exprectedResult)
 
-    def test_one(self):
+
+
+    def test_3(self):
         self.run_test([[5,4],[6,4],[6,7],[2,3]], 3)
 
-    def test_two(self):
+    def test_4(self):
         self.run_test([[1,1],[1,1],[1,1]], 1)
 
-    def test_three(self):
+    def test_5(self):
         self.run_test(large_testcase, 127) 
 
+    def test_2(self):
+        self.run_test([[46,89],[50,53],[52,68],[72,45],[77,81]], 3)
+
+    def test_1(self):
+        self.run_test([[1,2],[2,3],[3,4],[3,5],[4,5],[5,5],[5,6],[6,7],[7,8]], 7)
 try:
     unittest.main()
 except SystemExit:
