2946870fdd9ac1805361e39ee5144b80aae24474
Andrew Shepherd <shepherd_a@hotmail.com>
2023-01-29 17:48:06 +1000

    Works but TLE

--- put-marbles-in-bags/put_marbles.py
+++ put-marbles-in-bags/put_marbles.py
@@ -1,26 +1,27 @@
-import heapq
+import math
+from functools import cache
 
 class Solution:
-    # How many discretionary numbers can you pick
-    def calc_q_length(k):
-        if k == 1:  #[n-n]
-            return 0
-        elif k == 2: #[n,0][1,n]
-            return 2
-        elif k == 3:
-            return 4 
-    
+
     def putMarbles(self, weights: list[int], k: int) -> int:
         
+        @cache
+        def solve_recursive(up_to, k, pick, initial):
+            if k == 1:
+                return weights[0] + weights[up_to-1]
+            if up_to == k:
+                return sum(weights[:up_to]) * 2
+            final_partition_possible_length = range(1,up_to-k+2)
+            value = initial
+            for l in final_partition_possible_length:
+                these_ends = (weights[up_to-l], weights[up_to-1])
+                sub_value = solve_recursive(up_to-l, k-1, pick, initial)
+                this_result = sub_value +sum(these_ends)
+                value = pick(value, this_result)
+            return value
 
-        queue_length = (k-1)*2
-        min_q = []
-        max_q = []
-        for w in weights:
-            if len(min_q) < queue_length:
-                heapq.heappush(min_q, w)
-                heapq.heappush(max_q, w)
-            else:
-                heapq.heappushpop(min_q, 0-w)
-                heapq.heappushpop(max_q, w)
-        return abs(sum(min_q) - sum(max_q))
+
+        max_result = solve_recursive(len(weights), k, max, 0)
+        min_result = solve_recursive(len(weights), k, min, math.inf)
+        return max_result - min_result
+
--- put-marbles-in-bags/test.py
+++ put-marbles-in-bags/test.py
@@ -5,18 +5,45 @@ from put_marbles import Solution
 class TestSolution(unittest.TestCase):
 
     def test_0(self):
+        weights = [1,3,5,1]
+        k = 2
+        s = Solution()
+        result = s.putMarbles(weights, k)
+        #[1][4][2,5,2] = (1+1) + (4+4)+(2+2) = 2 + 8 + 4 = 12
+        #[1, 4, 2][5][2] = (5+5) + (2) + (3) = 15
+        # 
+        # (5+4+2+2) - (1+2+2+4) =  
+        self.assertEqual(4, result)
+
+
+    def test_6(self):
         weights = [1,4,2,5,2]
         k = 3
         s = Solution()
         result = s.putMarbles(weights, k)
-        #[1][4][2,5,2] = 9   
-        #[1][4, 2], 5[2] = 12
+        #[1][4][2,5,2] = (1+1) + (4+4)+(2+2) = 2 + 8 + 4 = 12
+        #[1, 4, 2][5][2] = (5+5) + (2) + (3) = 15
         # 
         # (5+4+2+2) - (1+2+2+4) =  
         self.assertEqual(3, result)
 
 
     def test_2(self):
+        weights = [54,6,34,66,63,52,39,62,46,75,28,65,18,37,18,13,33,69,19,40,13,10,43,61,72]
+        k = 4
+        s = Solution()
+        result = s.putMarbles(weights, k)
+        self.assertEqual(289, result)
+
+    def test_3(self):
+        weights = [1,2]
+        k = 1
+        s = Solution()
+        result = s.putMarbles(weights, k)
+        self.assertEqual(0, result)
+
+
+    def test_1(self):
         weights = [1,2,3,4,5]
         k = 1
         s = Solution()
