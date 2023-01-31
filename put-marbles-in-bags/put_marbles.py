198b8bf565967501766b08fbf1db57cc6a742069
Andrew Shepherd <shepherd_a@hotmail.com>
2023-01-31 10:50:59 +1000

    Faster, still TLE

--- put-marbles-in-bags/put_marbles.py
+++ put-marbles-in-bags/put_marbles.py
@@ -1,30 +1,23 @@
 import math
-from functools import cache
 
 class Solution:
 
     def putMarbles(self, weights: list[int], k: int) -> int:
-
         dp_lengths = len(weights)-k+1
-        start_val = (math.inf, -math.inf)
-        dp = [start_val] * dp_lengths
+        dp = [None] * dp_lengths
         for i in range(len(dp)):
             score = weights[0] + weights[i]
-            dp[i] = (score, score)
-        
+            dp[i] = (score, score)        
         for offset in range(1, k):
-            dp2 = [start_val] * dp_lengths
+            dp2 = [None] * dp_lengths
+            min_start = math.inf
+            max_start = -math.inf
             for i in range(len(dp2)):
-                best_min, best_max = start_val
-                for j in range(i+1):
-                    prev_min, prev_max = dp[j]
-                    this_start, this_end = (weights[j + offset], weights[i + offset])
-                    this_val = this_start + this_end
-                    this_min = prev_min + this_val
-                    this_max = prev_max + this_val
-                    best_min = min(best_min, this_min)
-                    best_max = max(best_max, this_max)
-                dp2[i] = (best_min, best_max)
+                prev_min, prev_max = dp[i]
+                this_end = weights[i + offset]
+                min_start = min(min_start, prev_min + this_end)
+                max_start = max(max_start, prev_max + this_end)
+                dp2[i] = (min_start + this_end , max_start + this_end)
             dp = dp2
         l,r = dp[-1]
         return r - l
--- put-marbles-in-bags/test.py
+++ put-marbles-in-bags/test.py
@@ -4,7 +4,7 @@ from put_marbles import Solution
 
 class TestSolution(unittest.TestCase):
 
-    def test_0(self):
+    def test_2(self):
         weights = [1,3,5,1]
         k = 2
         s = Solution()
@@ -16,19 +16,32 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(4, result)
 
 
-    def test_6(self):
+    def test_0(self):
         weights = [1,4,2,5,2]
         k = 3
         s = Solution()
-        result = s.putMarbles(weights, k)
+
+
+        # k = 1
+        # [1], [1,4], [1,2]
+
+        # k = 2
+        # min [1][4]  (10) # max [1,4] (10)
+        # min [1][4,2]=8 # max [1,4][2] = 9
+        # min [1][4,2,5] = 10 # max [1,4,2][5] = 13  
+        
+
         #[1][4][2,5,2] = (1+1) + (4+4)+(2+2) = 2 + 8 + 4 = 12
         #[1, 4, 2][5][2] = (5+5) + (2) + (3) = 15
         # 
         # (5+4+2+2) - (1+2+2+4) =  
+
+        result = s.putMarbles(weights, k)
+
         self.assertEqual(3, result)
 
 
-    def test_2(self):
+    def test_6(self):
         weights = [54,6,34,66,63,52,39,62,46,75,28,65,18,37,18,13,33,69,19,40,13,10,43,61,72]
         k = 4
         s = Solution()
