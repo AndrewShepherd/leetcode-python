4c71849db90bb81869e49cca87ebef176116c212
Andrew Shepherd <shepherd_a@hotmail.com>
2023-02-26 12:28:04 +1000

    Made it work

--- min-deviation-in-array/min_deviation_in_array.py
+++ min-deviation-in-array/min_deviation_in_array.py
@@ -1,34 +1,25 @@
+import heapq
+import math
+
 class Solution:
     def minimumDeviation(self, nums: list[int]) -> int:
-        nums = set(nums)
-        lower = min(nums)
-        higher = max(nums)
-        prev_min_maxes = set()
-        while(True):
-            if lower == higher:
-                return 0
-            if (lower,higher) in prev_min_maxes:
-                print(f"{(lower,higher)} in {prev_min_maxes}")
-                return higher - lower
-            prev_min_maxes.add((lower,higher))
-            print(f"nums = {sorted(nums)}")
+        q = []
+        min_val = math.inf
+        for n in nums:
+            if n%2:
+                n *= 2
+            min_val = min(min_val, n)
+            heapq.heappush(q, 0-n)
 
-            nums.remove(lower)
-            nums.remove(higher)
-            diff_before = higher - lower
-            if higher % 2 == 0:
-                higher //= 2
-            elif lower % 2 == 1:
-                lower *= 2
-            else:
-                return diff_before
-            if abs(higher - lower) >= diff_before:
-                return diff_before
-            nums.add(higher)
-            nums.add(lower)
-            lower = min(nums)
-            higher = max(nums)
- #           diff_after = higher - lower
- #           if diff_after >= diff_before:
- #               return diff_before
+        min_deviation = math.inf
+        while q:
+            n = 0 - heapq.heappop(q)
+            min_deviation = min(min_deviation, abs(n - min_val))
+            if n % 2:
+                break
+            n //= 2
+            min_val = min(min_val, n)
 
+            heapq.heappush(q, 0 - n)
+        return min_deviation
+            
