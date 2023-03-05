df196a7ca673ab02ddd515db9525a1cd7eabdf56
Andrew Shepherd <shepherd_a@hotmail.com>
2023-03-05 12:25:11 +1000

    Working now I believe

--- max-number-of-unmarked-indices/max_number_of_marked_indices.py
+++ max-number-of-unmarked-indices/max_number_of_marked_indices.py
@@ -1,27 +1,19 @@
-import bisect
+import heapq
 
 class Solution:
     def maxNumOfMarkedIndices(self, nums: list[int]) -> int:
         nums.sort()
-        print(f"{nums}")
-        marked = [False] * len(nums)
-        left_top = len(nums)
-
-        count = 0
-        for i in range(len(nums)-1, -1, -1):
-            if marked[i]:
-                continue
-            r = nums[i]
-            lIndex = bisect.bisect_right(nums, r//2, 0, left_top)
-            while(lIndex >= 0):
-                if not marked[lIndex] and (nums[lIndex] <= r//2):
-                    marked[lIndex] = True
-                    marked[i] = True # unnecessary
-                    count += 2
-                    left_top = lIndex
-                    print(f"{nums[lIndex]} - {nums[i]}")
+        l = len(nums)
+        left_half = nums[0:l//2]
+        right_half = nums[l//2:]
+        heapq.heapify(right_half)
+        c = 0
+        for n in left_half:
+            if not right_half:
+                break
+            while(right_half):
+                n2 = heapq.heappop(right_half)
+                if n2 >= n*2:
+                    c += 2
                     break
-                lIndex -= 1
-
-        remaining = [nums[i] for i in range(len(nums)) if not marked[i]]
-        return count
+        return c
