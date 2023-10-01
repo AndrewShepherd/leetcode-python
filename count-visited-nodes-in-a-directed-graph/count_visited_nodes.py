3fc6c26e1539f7032494440f4dd454760946fe61
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2023-10-02 06:59:40 +1000

    Complete rewrite, now it's fast

--- count-visited-nodes-in-a-directed-graph/count_visited_nodes.py
+++ count-visited-nodes-in-a-directed-graph/count_visited_nodes.py
@@ -1,49 +1,30 @@
 class Solution:
     def countVisitedNodes(self, edges: list[int]) -> list[int]:
-        # breadth first search from 0
-        def get_cycle(start_index):
-            cycle_list = [start_index]
-            this_index = start_index
-            while (True):
-                next = edges[this_index]
-                if next == start_index:
-                    return cycle_list
-                else:
-                    cycle_list.append(next)
-                    this_index = next
-
-
-        result = []
-        cycle_length = [None] * len(edges)
         result = [None] * len(edges)
         for i in range(len(edges)):
             if (result[i] != None):
                 continue
             stack = [i]
-            current = i
-            visited = [False] * len(edges)
+            d = dict()
+            d[i] = len(stack) - 1
             while(True):
-                visited[current] = True
-                next = edges[current]
-                if (result[next] != None):
-                    result[current] = result[next] + 1
-                    while(stack):
-                        e = stack.pop()
-                        if result[e] == None:
-                            result[e] = 1 + result[edges[e]]  
-                    break                 
-                if visited[next]:
-                    cycle = get_cycle(next)
-                    for e in cycle:
-                        result[e] = len(cycle)
-                    while(stack):
-                        e = stack.pop()
-                        if result[e] == None:
-                            result[e] = 1 + result[edges[e]]
+                next = edges[stack[-1]]
+                if result[next] != None:
+                    for e in reversed(stack):
+                        result[e] = result[edges[e]] + 1
+                    break
+                elif next in d:
+                    # We have a cycle
+                    sIndex = d[next]
+                    cycle_length = len(stack) - sIndex
+                    for j in range(sIndex, len(stack)):
+                        result[stack[j]] = cycle_length
+                    for j in range(sIndex-1, -1, -1):
+                        result[stack[j]] = result[edges[stack[j]]] + 1
                     break
                 else:
-                    stack.append(current)
-                    current = next
+                    stack.append(next)
+                    d[next] = len(stack) - 1
 
         return result
 
--- count-visited-nodes-in-a-directed-graph/test.py
+++ count-visited-nodes-in-a-directed-graph/test.py
@@ -17,6 +17,12 @@ class TestSolution(unittest.TestCase):
         result = s.countVisitedNodes(edges)
         self.assertEqual([5,5,5,5,5], result)
 
+    def test_0(self):
+        s = Solution()
+        edges = [8,17,14,8,14,12,16,11,4,14,19,6,8,8,2,10,2,1,1,18]
+        result = s.countVisitedNodes(edges)
+        self.assertEqual([5, 2, 2, 5, 3, 6, 4, 6, 4, 3, 5, 5, 5, 5, 2, 6, 3, 2, 3, 4], result)
+
 try:
     unittest.main()
 except SystemExit:
