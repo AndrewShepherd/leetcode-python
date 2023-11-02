31d98f8ea1d7afb944658c9aa4340ca902249137
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2023-11-02 16:18:56 +1000

    Got this towork,and fast enough too

--- max-points-after-collecting-coins-from-all-nodes/max_points_after_collecting_coins_from_all_nodes.py
+++ max-points-after-collecting-coins-from-all-nodes/max_points_after_collecting_coins_from_all_nodes.py
@@ -2,29 +2,73 @@ from collections import defaultdict
 from functools import cache
 from math import floor
 
+def maxify(result):
+    m = 0
+    for i in range(len(result)-1, -1, -1):
+        m = max(m, result[i])
+        result[i] = m
+    return result
+
+def safe_get(l, i):
+    if i < len(l):
+        return l[i]
+    else:
+        return 0
+
+def add_lists(l1, l2):
+    len1 = len(l1)
+    len2 = len(l2)
+    max_len = max(len1, len2)
+    return [safe_get(l1, i) + safe_get(l2, i) for i in range(max_len)] 
+
+def generate_sorted_list(edges: list[list[int]], n: int) -> list[tuple]:
+    relationships = defaultdict(list)
+
+    for node1, node2 in edges:
+        relationships[node1].append(node2)
+        relationships[node2].append(node1)
+
+    visited = [False] * n
+    visited[0] = True
+    sorted_list = [(0,None)]
+    i = 0
+    while i < len(sorted_list):
+        node, parent = sorted_list[i]
+        child_indexes = [c for c in relationships[node] if not visited[c]]
+        for c in child_indexes:
+            visited[c] = True
+            sorted_list.append((c, node))
+        i += 1
+    return sorted_list
+
+
 class Solution:
     def maximumPoints(self, edges: list[list[int]], coins: list[int], k: int) -> int:
-        relationships = defaultdict(list)
+        sorted_list = generate_sorted_list(edges, len(coins))
+        dp = [None] * len(coins)
 
-        for node1, node2 in edges:
-            relationships[node1].append(node2)
-            relationships[node2].append(node1)
-        
-        visited = [False] * len(coins)
-        @cache
-        def best_result(node_index, divisions):
-            visited[node_index] = True
-            adjacent_nodes = [i for i in relationships[node_index] if not visited[i]]
-            child_results_divisions = []
-            if divisions < 14:
-                child_results_divisions = [best_result(child_index, divisions+1) for child_index in adjacent_nodes]
-            child_results_division = sum(child_results_divisions)
-            no_divisions = [best_result(child_index, divisions) for child_index in adjacent_nodes]
-            child_results_no_division = sum(no_divisions)
-            visited[node_index] = False
-            return max(
-                floor(coins[node_index]/(2**divisions)) - k + child_results_no_division,
-                floor(coins[node_index]/(2**(divisions+1))) + child_results_division
-            )
-        
-        return best_result(0, 0)
+        while(sorted_list):
+            node, parent = sorted_list.pop()
+            existing = [] if dp[node] == None else dp[node]
+            
+            these_scores = []
+            these_coins = coins[node]
+            i = 0
+            while (True):
+                this_value = max(
+                    these_coins - k + safe_get(existing, i),
+                    floor(these_coins / 2) + safe_get(existing, i + 1)
+                )
+                if this_value == 0:
+                    break
+                these_scores.append(this_value)
+                these_coins = floor(these_coins / 2)
+                i += 1
+
+            if (parent == None):
+                return these_scores[0] if len(these_scores) else 0
+            else:
+                if dp[parent] == None:
+                    dp[parent] = these_scores
+                else:
+                    dp[parent] = add_lists(dp[parent], these_scores)
--- max-points-after-collecting-coins-from-all-nodes/test.py
+++ max-points-after-collecting-coins-from-all-nodes/test.py
@@ -5,7 +5,7 @@ from max_points_after_collecting_coins_from_all_nodes import Solution
 
 class TestSolution(unittest.TestCase):
 
-    def test_1(self):
+    def test_0(self):
         s = Solution()
         edges = [[0,1],[1,2],[2,3]]
         coins = [10,10,3,3]
@@ -21,20 +21,19 @@ class TestSolution(unittest.TestCase):
         result = s.maximumPoints(edges, coins, k)
         self.assertEqual(16, result)
 
-    def test_0(self):
+    def test_1(self):
         s = Solution()
         edges = [[0,1],[2,0],[0,3],[4,2]]
         coins = [7,5,0,9,3]
         k = 4
-
-        # The tree is rooted at node 0!
-        
         #
-        # (4, 3)
+        # (0, 1)
         #  |
-        # (2, 0)
+        #  +-- (2, 0)
+        #  |     |
+        #  |     +--- (4, 3)
         #  |
-        # (0, )
+        #  +-- (3, 9)
 
 
         result = s.maximumPoints(edges, coins, k)
