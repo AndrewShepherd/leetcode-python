a9f387fa5e7705a724593dae5c9de2504bda04f1
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2024-02-11 12:29:53 +1000

    Looks good now

--- minimize-or-of-remaining-elements-using-operations/minimize_or_of_remaining_elements_using_operations.py
+++ minimize-or-of-remaining-elements-using-operations/minimize_or_of_remaining_elements_using_operations.py
@@ -1,30 +1,26 @@
-bit_count = 30
-all_bits = (1 << bit_count) - 1
+all_bits = 0xFFFFFFFF
 
-def bits_to_right_of(bit):
-    return (1 << bit) - 1
+def iterate_through_bits():
+    less_significant_bits = all_bits
+    b = 1 << 31
+    while(b):
+        less_significant_bits &= ~b
+        yield b,less_significant_bits
+        b >>= 1
 
 class Solution:
     def minOrAfterOperations(self, nums: list[int], k: int) -> int:
-        # this will contain the 'or' of all final elements of array
-        ans = 0
-        # Iterating from Most Significant Bit to Least Significant Bit
-        for bit in range(bit_count - 1, -1, -1):
-            # target: represents the set of bits we are not concerned with for this iteration
-            target = (ans | bits_to_right_of(bit))
-            elements_that_dont_need_to_be_swapped = 0 
-            # cur: The set of bits that will NOT be cancelled out, 
-            #      given whether the current element is swapping with the previous element 
-            #      or not
-            cur = all_bits
-            for num in nums:
-                cur &= num
-                if cur | target == target:  # There is nothing in this group we care about
-                    elements_that_dont_need_to_be_swapped += 1
-                    cur = all_bits
-            # we have to keep the current bit if (len(nums)-count) is greater than k 
-            # otherwise we can remove current bit in less than or equal to k operations
-            swaps_required = len(nums) - elements_that_dont_need_to_be_swapped
-            if swaps_required> k:
-                ans |= (1 << bit)
-        return ans
+        bits_we_cant_remove = 0
+        for b, less_significant_bits in iterate_through_bits():
+            unswapped_count = 0
+            bits_not_cancelled = all_bits
+            irrelevant_bits = bits_we_cant_remove | less_significant_bits
+            for n in nums:
+                bits_not_cancelled = bits_not_cancelled & n
+                if (bits_not_cancelled | irrelevant_bits) == irrelevant_bits:
+                    unswapped_count += 1
+                    bits_not_cancelled = all_bits
+            swaps_required = len(nums) - unswapped_count
+            if (swaps_required > k):
+                bits_we_cant_remove |= b
+        return bits_we_cant_remove
