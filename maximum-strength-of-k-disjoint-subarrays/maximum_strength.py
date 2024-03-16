ab62f455dcca99bb2190342444f18995ac963d6c
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2024-03-16 10:15:28 +1000

    Made it fast

--- maximum-strength-of-k-disjoint-subarrays/maximum_strength.py
+++ maximum-strength-of-k-disjoint-subarrays/maximum_strength.py
@@ -1,29 +1,21 @@
+from array import array
+
 class Solution:
     def maximumStrength(self, nums: list[int], k_total: int) -> int:
-        dp_any_range = [0] * len(nums)
-
-        # solve it for 1
-        last_selected = nums[0] * k_total
-        dp_any_range[0] = last_selected
-        for i in range(1, len(nums)):
-            n = nums[i] * k_total
-            last_selected = max(last_selected + n, n)
-            dp_any_range[i] = max(last_selected, dp_any_range[i-1])
-
-        # solve it for the rest
-        for k in range(1, k_total):
+        dp_length = len(nums) - k_total + 1
+        dp = [0] * dp_length
+        dp_prev = [0] * dp_length
+        for k in range(k_total):
+            source_index = k
+            dp_prev, dp = dp, dp_prev
             s = -1 if k%2 else 1
             m = (k_total - k)
             n = nums[k] * m * s
-            
-            dp_any_range_prev = dp_any_range
-            dp_any_range = [0] * len(nums)
-            last_selected = dp_any_range_prev[k-1] + n
-            dp_any_range[k] = last_selected
-            for i in range(k+1, len(nums)):
-                n = nums[i] * m * s
-                last_selected = max(last_selected + n, dp_any_range_prev[i-1] + n)
-                dp_any_range[i] = max(last_selected, dp_any_range[i-1])
-
-        return dp_any_range[-1]
-        
+            last_selected = dp_prev[0] + n
+            dp[0] = last_selected
+            for i in range(1, dp_length):
+                source_index = i + k
+                n = nums[source_index] * m * s
+                last_selected = max(last_selected + n, dp_prev[i] + n)
+                dp[i] = max(last_selected, dp[i-1])
+        return dp[-1]
