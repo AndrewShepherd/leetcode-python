f8189d807a1d9586c5f767c2ea1d1760655eda01
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2024-11-18 12:22:40 +1000

    Building the tree a different way

--- zero-array-transformation-II/test.py
+++ zero-array-transformation-II/test.py
@@ -4,6 +4,22 @@ import unittest
 class TestSolution(unittest.TestCase):
 
     def test_00(self):
+        nums = [1,4,2,7]
+        queries = [[3,3,2],[0,1,3],[0,2,1],[0,2,2],[3,3,5],[2,3,2],[1,2,2]]
+        expectedResult = 5
+        s = Solution()
+        result = s.minZeroArray(nums, queries)
+        self.assertEqual(expectedResult, result)
+
+    def test_07(self):
+        nums = [0,10]
+        queries = [[0,1,2],[0,0,2],[0,1,2],[1,1,4],[0,1,3],[1,1,4],[0,1,2],[0,1,2],[0,1,2],[0,0,2],[1,1,2],[0,0,2],[0,0,3],[1,1,3],[0,0,5]]
+        expectedResult = 5
+        s = Solution()
+        result = s.minZeroArray(nums, queries)
+        self.assertEqual(expectedResult, result)
+
+    def test_06(self):
         nums = [6,10,0,0]
         queries = [[0,0,3],[0,2,1],[3,3,1],[1,1,4],[1,3,2],[3,3,3],[0,0,5],[0,1,5],[0,2,1],[1,1,1],[0,3,1],[0,2,4],[1,3,4],[1,2,5],[1,2,3]]
         expectedResult = 8
--- zero-array-transformation-II/zero_array_transformation_II.py
+++ zero-array-transformation-II/zero_array_transformation_II.py
@@ -1,18 +1,10 @@
-def create_node(range_start, range_end_exclusive, index, value):
-    if range_end_exclusive - range_start == 1:
-        if value == 0:
-            return EmptyNode()
-        else:
-            return ValueNode(value)
-    else:
-        return BranchNode(range_start, range_end_exclusive, None, None).set_value(index, value)
-
-class InitialNode:
-    def __init__(self, count):
-        self.count = count
-
-    def set_value(self, index, value):
-        return BranchNode(0, self.count, None, None).set_value(index, value)
+class EmptyNode:
+    def __init__(self):
+        self.max_value = 0
+    def subtract(self, range_start, range_end_exclusive, value):
+        return self
+    
+EMPTY = EmptyNode()
     
 class BranchNode:
     def __init__(self, range_start, range_end_exclusive, left_node, right_node, subtract_amount = 0):
@@ -21,13 +13,13 @@ class BranchNode:
         self.left_node = left_node
         self.right_node = right_node
         self.subtract_amount = subtract_amount
-        max_child_value = max(0 if self.left_node == None else self.left_node.max_value, 0 if self.right_node == None else self.right_node.max_value)
+        max_child_value = max(self.left_node.max_value, self.right_node.max_value)
         self.max_value = max_child_value - subtract_amount
 
     def subtract(self, range_start, range_end_exclusive, value):
         if range_start <= self.range_start and range_end_exclusive >= self.range_end_exclusive:
             if value >= self.max_value:
-                return EmptyNode()
+                return EMPTY
             else:
                 return BranchNode(self.range_start, self.range_end_exclusive, self.left_node, self.right_node, self.subtract_amount + value)
         else:
@@ -38,52 +30,44 @@ class BranchNode:
                 left_node = left_node.subtract(range_start, min(mid_point, range_end_exclusive), value)
             if range_end_exclusive > mid_point:
                 right_node = right_node.subtract(max(mid_point, range_start), range_end_exclusive, value)
-            max_child_value = max(0 if left_node == None else left_node.max_value, 0 if right_node == None else right_node.max_value)
+            max_child_value = max(left_node.max_value, right_node.max_value)
             if max_child_value - self.subtract_amount <= 0:
-                return EmptyNode()
+                return EMPTY
             else:
                 return BranchNode(self.range_start, self.range_end_exclusive, left_node, right_node, self.subtract_amount)
 
-    def set_value(self, index, value):
-        mid_point = (self.range_start + self.range_end_exclusive) // 2
-        left_node = self.left_node
-        right_node = self.right_node
-        if index < mid_point:
-            if self.left_node == None:
-                left_node = create_node(self.range_start, mid_point, index,value)
-            else:
-                left_node = self.left_node.set_value(index, value)
-        else:
-            if self.right_node == None:
-                right_node = create_node(mid_point, self.range_end_exclusive, index, value)
-            else:
-                right_node = self.right_node.set_value(index, value)
-        return BranchNode(self.range_start, self.range_end_exclusive, left_node, right_node, self.subtract_amount)
-    
 class ValueNode:
     def __init__(self, value):
         self.max_value = value
 
     def subtract(self, range_start, range_end_exclusive, value):
         if value >= self.max_value:
-            return EmptyNode()
+            return EMPTY
         else:
             return ValueNode(self.max_value - value)
-
-class EmptyNode:
-    def __init__(self):
-        self.max_value = 0
-    def subtract(self, range_start, range_end_exclusive, value):
-        return self
 
 
 class Solution:
     def minZeroArray(self, nums: list[int], queries: list[list[int]]) -> int:
-        if all([n == 0 for n in nums]):
+        # Build the tree
+        nodes = [ValueNode(n) for i,n in enumerate(nums)]
+        m = 1
+        while(len(nodes) > 1):
+            nodes2 = []
+            m *= 2
+            for i in range(0, len(nodes), 2):
+                left_node = nodes[i]
+                right_node = nodes[i+1] if i+1 < len(nodes) else EMPTY
+                range_start = len(nodes2) * m
+                range_end = (len(nodes2) + 1) * m
+                nodes2.append(BranchNode(range_start, range_end, left_node, right_node, 0))
+            nodes = nodes2
+
+        node = nodes[0]
+
+        if node.max_value == 0:
             return 0
-        node = InitialNode(len(nums))
-        for i,n in enumerate(nums):
-            node = node.set_value(i, n)
+
         for i,(range_start, range_end, value) in enumerate(queries):
             node = node.subtract(range_start, range_end + 1, value)
             if node.max_value == 0:
