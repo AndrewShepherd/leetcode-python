609146070d4bd62e1f078e464c2832b045e47f6a
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2025-11-20 17:35:26 +1000

    Much faster again

--- max-cyclic-partition-score/max_cyclic_partition_score.py
+++ max-cyclic-partition-score/max_cyclic_partition_score.py
@@ -1,6 +1,4 @@
-from functools import cache
-from math import inf;
-
+from math import inf
 
 start_as_max = 0
 start_as_min = 1
@@ -15,39 +13,26 @@ class Solution:
             if range_length < 2:
                 return 0
             range_start %= len(nums)
-            dp = [
-                [-inf, -inf, 0] for i in range(k)
-            ]
+            no_previous_values = [-inf, -inf, 0]
+            dp = [no_previous_values] * k
             for i in range(range_length):
-                n = nums[(range_start + i)% len(nums)]
-                dp2 = [None] * k
-                for j in range(len(dp)):
+                n = nums[(range_start + i) % len(nums)]
+                dp2 = [no_previous_values] * k
+                for j in range(min(len(dp), i//2+1)):
                     this_entry = dp[j]
-                    previous_entry = None if j == 0 else dp[j-1]
-                    if previous_entry == None:
-                        dp2[j] = [
-                            max(n, this_entry[start_as_max]),
-                            max(0 - n, this_entry[start_as_min]),
-                            max(
-                                this_entry[finish_partition],
-                                this_entry[start_as_max] - n,
-                                this_entry[start_as_min] + n
-                            )
-                        ]
-                    else:
-                        dp2[j] = [
-                            max(previous_entry[finish_partition] + n, this_entry[start_as_max]),
-                            max(previous_entry[finish_partition] - n, this_entry[start_as_min]),
-                            max(
-                                this_entry[finish_partition],
-                                this_entry[start_as_max] - n,
-                                this_entry[start_as_min] + n
-                            )
-                        ]
+                    previous_entry = no_previous_values if j == 0 else dp[j-1]
+                    dp2[j] = [
+                        max(previous_entry[finish_partition] + n, this_entry[start_as_max]),
+                        max(previous_entry[finish_partition] - n, this_entry[start_as_min]),
+                        max(
+                            this_entry[finish_partition],
+                            this_entry[start_as_max] - n,
+                            this_entry[start_as_min] + n
+                        )
+                    ]
                 dp = dp2
             return max([e[finish_partition] for e in dp])
 
-        result = 0
         _, max_index = max([(v,i) for i,v in enumerate(nums)])
         return max(
             best_result(max_index, len(nums), k), 
