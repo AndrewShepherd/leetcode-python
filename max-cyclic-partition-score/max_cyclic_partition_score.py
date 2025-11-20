8082299b82cff77657f9dacf775c8726c6733e0c
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2025-11-20 11:11:18 +1000

    O(n*n*k). Too slow

--- max-cyclic-partition-score/max_cyclic_partition_score.py
+++ max-cyclic-partition-score/max_cyclic_partition_score.py
@@ -3,35 +3,36 @@ from functools import cache
 
 class Solution:
     def maximumScore(self, nums: list[int], k: int) -> int:
+       
         @cache 
-        def best_result(array_start, this_partition_start, available_partitions):
+        def best_result(range_start, range_length, available_partitions):
             if (available_partitions == 0):
                 return 0
-            partition_length = 1
-            i = this_partition_start
-            min_value = nums[i]
-            max_value = nums[i]
-            result = 0
-            best_result_for_this_only = 0
-            while (True):
-                i = (i+1)%len(nums)
-                if i == array_start:
-                    break
-                min_value = min(min_value, nums[i])
-                max_value = max(max_value, nums[i])
-                diff = max_value - min_value
-                if diff > best_result_for_this_only:
-                    best_result_for_this_only = diff
-                    if (i+1)%len(nums) != array_start:
-                        result = max(
-                            result, 
-                            diff + best_result(array_start, (i+1)%len(nums), available_partitions-1)
-                        )
+            if range_length < 2:
+                return 0
+            range_start %= len(nums)
+            max_so_far = nums[range_start]
+            min_so_far = nums[range_start]
+            biggest_range = 0
+            result =  0
+            for i in range(1, range_length):
+                index = (range_start + i) % len(nums)
+                max_so_far = max(max_so_far, nums[index])
+                min_so_far = min(min_so_far, nums[index])
+                this_range = max_so_far - min_so_far
+                if (this_range > biggest_range):
+                    biggest_range = this_range
+                    if available_partitions > 0:
+                        all_the_rest = best_result(index + 1, range_length - i - 1, available_partitions - 1)
+                        result = max(result, biggest_range + all_the_rest)
                     else:
-                        result = max(result, diff)
+                        result = biggest_range
             return result
 
         result = 0
-        for i in range(len(nums)):
-            result = max(result, best_result(i, i, k))
-        return result
+        _, max_index = max([(v,i) for i,v in enumerate(nums)])
+        return max(
+            best_result(max_index, len(nums), k), 
+            best_result(max_index + 1, len(nums), k)
+        )
+
